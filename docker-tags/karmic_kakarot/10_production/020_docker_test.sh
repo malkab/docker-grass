5914a6f0661aeee066d90bafe4038ceceacbd5f5
Malkab <jp.perez.alcantara@gmail.com>
2024-09-04 14:13:04 +0200

    wip

--- docker-tags/karmic_kakarot/10_production/020_docker_test.sh
+++ docker-tags/karmic_kakarot/10_production/020_docker_test.sh
@@ -5,13 +5,15 @@
 
 # -----------------------------------------------------------------
 #
-# Starts a PG container
+# Run a container with X server access for graphical GRASS.
 #
 # -----------------------------------------------------------------
+xhost +local:docker
+
 docker run -ti --rm \
     --name grass_production_test \
-    --network=host \
     --user 1000:1000 \
     -e DISPLAY=$DISPLAY \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v $(pwd):/source \
     malkab/grass:${DOCKER_IMAGE_TAG}
